35c59c3e79b71e4aef036dec92000124713a2a44
timathom <timathom@gmail.com>
2017-12-14 11:04:15 -0500

    Update training data.

--- project/neuralnet/neuralnet.py
+++ project/neuralnet/neuralnet.py
@@ -1,7 +1,16 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Implements the supervised gradident-based learning algorithm specified by 
+Created on Thu Dec  7 03:27:25 2017
+
+@author: tat2
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Based on the supervised gradident-based learning algorithm specified by 
 Reyes-Galaviz et al. (2017).
 
 """
@@ -9,22 +18,22 @@ Reyes-Galaviz et al. (2017).
 import numpy as np
 
 input_count = 8 
-hidden_count = 6 
+hidden_count = 5
 output_count = 1
 
-eta = 0.8
+eta = 0.04
 
-data = np.load("record_pairs.npy")
+data = np.load("record_pairs.npy", encoding="latin1")
 data2 = np.nan_to_num(data)
 
 # Quality thresholds
-tau_1 = 0.4
-tau_2 = 0.7
+tau_1 = 0.99
+tau_2 = 0.01
 
 # The MLP model has 8 inputs and 5 nodes in the hidden layer. It follows 
 # the Model 2 setup presented by Reyes-Galaviz et al. Hidden nodes aggregate
 # values calculated by running each of 8 comparison functions over 5 data
-# fields (date, ISBN, title, creator, contributor)
+# fields (date, ISBN, title, publisher, creator/contributor)
 r = np.random.randn(input_count, hidden_count)
 w = np.random.randn(hidden_count, output_count)
 
@@ -33,63 +42,102 @@ def activate(a):
     return 1 / (1 + np.exp(-a))
 
 # Partial derivatives for output layer
-def Q_tau_1(s):    
-    return np.sum(s**2)
 
-def Q_tau_2(s):
-    return np.sum((1 - s)**2)
+# MSE
+#def E_total(c, s):
+#    if c == 0:
+#        return 0.5 * np.sum(tau_1 - s)**2
+#    else:
+#        return 0.5 * np.sum(tau_2 - s)**2
+    
+#def dQ_ds(c, s):
+#    if c == 0:
+#        return -(tau_1 - np.sum(s))
+#    else:
+#        return -(tau_2 - np.sum(s))             
+    
+def dQ_ds(c, data):    
+    total = []
+    for d in data:            
+        if c == 0:
+            if np.sum(d) > tau_1:
+                d = 2 * d
+        else:
+            if np.sum(d) < tau_2:
+                d = 2 * (d - 1)
+        total.append(d)
+    return np.sum(total)
 
-def Q(d, s, tau_1, tau_2):
-    if d[6:7, 1:2] == 0:        
-        return Q_tau_1(s)
-    else:
-        return Q_tau_2(s)
-
-def dQ_ds(d, s, tau_1, tau_2):    
-    if d[6:7, 1:2] == 0:        
-        return 2 * s
-    else:
-        return 2 * (s - 1)        
 
 def ds_dl(s):
     return s * (1 - s)
 
-# Partial derivatives for hidden layer
 def dy_dp(y):
     return y * (1 - y)
+ 
+output = []
 
-# 150 iterations for training model
-for i in range(150):      
-    
-    for u in data2:
-    
-        # Net input of hidden layer
-        p = np.dot(u[:6, :], r)        
-        
-        # Activation of hidden layer
-        y = activate(p)
-        
-        # Net input of output node
-        l = np.dot(y, w)        
-        
-        # Activation of output node
-        s = activate(l)              
-        
-        # Chain of partial derivatives for Model 2
-        # dQ_ds = dQ_ds(u, s, tau_1, tau_2)
-        # ds_dl = ds_dl(s)
-        dl_dw = y.T
-        dl_dy = w.T
-        # dQ_dl = dQ_ds * ds_dl
-        # dQ_dy = dQ_dl * dl_dy        
-        # dy_dp = dy_dp(y)
-        dp_dr = u[:6, :].T
-        
-        # Back propogate                
-        dQ_dw = np.dot(dl_dw, (dQ_ds(u, s, tau_1, tau_2) * ds_dl(s)))        
-        dQ_dr = np.dot(dp_dr, (dQ_ds(u, s, tau_1, tau_2) * ds_dl(s) * dl_dy * dy_dp(y)))        
-                             
-        r -= dQ_dr * eta              
-        w -= dQ_dw * eta
+training = data[0:1960]
 
-        print(np.sum(s), u[6:7, 1:2])
+# Iterations for training model
+for i in range(1000):      
+      
+    training = np.random.permutation(training)    
+    
+    for t in training:    
+        t2 = np.nan_to_num(t)
+        for u in t2:                                     
+            c = u[:, :][5][0]
+        
+            u2 = u[:5, :]
+        
+            # Net input of hidden layer
+            p = np.dot(u2, r)        
+            
+            # Activation of hidden layer
+            y = activate(p)
+            
+            # Chain of partial derivatives for Model 2               
+            
+            # Net input of output node
+            l = np.dot(y, w)        
+            
+            # Activation of output node
+            s = activate(l)   
+            
+            dE_o = dQ_ds(c, s)
+            
+            dE_h = np.dot(dE_o, w.T)
+            
+            dQ_dw = eta * np.dot(y.T, dE_o * ds_dl(s))
+                                 
+            dQ_dr = eta * np.dot(u2.T, dE_h * dy_dp(y))        
+            
+            r -= dQ_dr
+            w -= dQ_dw
+            
+            output.append([np.sum(s), c])
+             
+    
+    result = np.array([output])
+                        
+        
+    f = result
+    if len(f) > 0:              
+        f2 = result[:, :, 1] == 0
+        f3 = result[:, :, 1] == 1
+        f4 = result[:, :, 0] > 0.5
+        f5 = result[:, :, 0] < 0.5
+          
+        print(len(f[f2 & f4]), len(f[f3 & f5]))
+        
+    r_final = r
+    w_final = w
+        
+    output = []
+    result = []
+    #np.savetxt("training_output.txt", np.array([output]))
+    
+    
+    
+    
